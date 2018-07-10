587d044ce746f9bdfb0996344bcf081f2c59fad2
Alex <alex.daigle2@gmail.com>
2018-07-10 11:26:43 -0400

    Added: Stop safari notificatinos for high sierra.

--- start.sh
+++ start.sh
@@ -22,6 +22,11 @@ brew install coreutils
 sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 sudo ln -s /usr/local/bin/gsha512sum /usr/local/bin/sha512sum
 
+# Stop Safari notifications. (COME ON APPLE! REALLY?!)
+defaults write com.apple.coreservices.uiagent CSUIHasSafariBeenLaunched -bool YES
+defaults write com.apple.coreservices.uiagent CSUIRecommendSafariNextNotificationDate -date 2050-01-01T00:00:00Z
+defaults write com.apple.coreservices.uiagent CSUILastOSVersionWhereSafariRecommendationWasMade -float 10.99
+
 # Setup bash settings.
 chmod +x ./reload.sh
 ./reload.sh
