b8f489d0873e3550279e5c67b9b1c8e1667e6310
Alex <alex.daigle2@gmail.com>
2018-10-17 00:45:30 -0400

    Added: vim

--- reload.sh
+++ reload.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 # Copy files over.
-cp ./.aliases ~/ && cp ./.bash_* ~/ && cp ./.functions ~/
+cp ./.aliases ~/ && cp ./.bash_* ~/ && cp ./.functions ~/ && cp ./.vimrc ~/
 
 # Refresh the settings.
 . ~/.aliases
 . ~/.bash_prompt
 . ~/.bash_profile
 . ~/.functions
+. ~/.vimrc
 
 # Update all brew packages, and cleanup old versions.
 brew upgrade
