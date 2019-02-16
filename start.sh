c646bf02aa949caf9db85b79dd595220f3643d48
deepio <alex.daigle2@gmail.com>
2019-02-16 15:31:10 -0500

    Fixed: peda with ssh

--- start.sh
+++ start.sh
@@ -38,7 +38,7 @@ brew install curl --with-openssl
 brew install wget
 cd ~/Documents/GitHub/ || return
 git clone git@github.com:slimm609/checksec.sh.git
-git clone https://github.com/longld/peda.git ~/peda
+git clone git@github.com:longld/peda.git ~/peda
 git clone git@github.com:sdispater/poetry.git
 python3 ~/poetry/get-poetry.py
 
