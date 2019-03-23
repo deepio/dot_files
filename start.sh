5f4bb3995b3e85eb62c1a2c02283469d9dd317ce
deepio <alex.daigle2@gmail.com>
2019-03-23 12:32:11 -0400

    Added: entr and ag

--- start.sh
+++ start.sh
@@ -36,6 +36,8 @@ brew install pstree
 brew install pypy3
 brew install curl --with-openssl
 brew install wget
+brew install entr
+brew install ag
 cd ~/Documents/GitHub/ || return
 git clone git@github.com:slimm609/checksec.sh.git
 git clone git@github.com:longld/peda.git ~/peda
