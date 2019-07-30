c06313f1dc4c960c528c4c4f7038b63e4d367c71
deepio <alex.daigle2@gmail.com>
2019-07-30 13:01:48 -0400

    Added: gitconfig

--- reload.sh
+++ reload.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
 # Copy files over.
-cp ./.aliases ~/ && cp ./.bash_* ~/ && cp ./.functions ~/ && cp ./.vimrc ~/ && cp ./.gdbinit ~/ 
+cp ./.aliases ~/ && cp ./.bash_* ~/ && cp ./.functions ~/ && cp ./.vimrc ~/ && cp ./.gdbinit ~/ && cp ./.gitconfig ~/
+
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	alias sed="gsed"
+fi
+
+echo "Enter GitHub Username and press [enter]: "
+read gh_user
+sed -i "s/name =/name = `echo $gh_user`/g" ~/.gitconfig
+
+echo "Enter GitHub Email and press [enter]: "
+read gh_email
+sed -i "s/email =/email = `echo $gh_email`/g" ~/.gitconfig
 
 # Refresh the settings.
 . ~/.aliases
