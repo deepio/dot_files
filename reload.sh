3a65fd00e604d5074b397f8f541573da35ff52e0
deepio <alex.daigle2@gmail.com>
2021-02-22 13:37:36 -0500

    gpg

--- reload.sh
+++ reload.sh
@@ -19,6 +19,18 @@ echo "Enter GitHub Email and press [enter]: "
 read gh_email
 sed -i "s/email =/email = `echo $gh_email`/g" ~/.gitconfig
 
+echo """
+Remember to run `git config --global user.signingkey` with the fingerprint you wish to use.\n
+
+You can see the fingerprint with `gpg --list-secret-keys --keyid-format LONG | grep rsa | grep sec`
+Everything after rsa####/ until the first space is your fingerprint. 
+
+	sec   rsa4096/123456ABCDEFGHIK 2021-02-18 [SC] [expires: 2022-02-18]
+
+Then your fingerprint is 123456ABCDEFGHIK and the command would be:
+git config --global user.signingkey 123456ABCDEFGHIK
+"""
+
 # Refresh the settings.
 . ~/.aliases
 # . ~/.bash_prompt
